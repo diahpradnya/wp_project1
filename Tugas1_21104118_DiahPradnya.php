b3c177a973de1c16107912485ff3611fd955cb1b
Diah Pradnya <101110652+diahpradnya@users.noreply.github.com>
2022-04-03 10:42:38 +0800

    setor tugas

--- Tugas1_21104118_DiahPradnya.php
+++ Tugas1_21104118_DiahPradnya.php
@@ -1,39 +1,35 @@
+<!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'>
-<title>Menghitung Luas Segitiga</title>
+    
+    <title>Menghitung Luas Segitigas</title>
 </head>
 <body>
-    <font color='purple'> Menghitung Luas Segitiga </font>
-    <table>
-        <tr>
-            <td>Alas</td>
-            <td>:</td>
-            <td><input type='text' id='txtAlas' name='txtAlas'></td>
-        </tr>
-        <tr>
-            <td>Tinggi</td>
-            <td>:</td>
-            <td><input type='text' id='txtTinggi' name='txtTinggi'></td>
-        </tr>
-        <tr>
-            <td><input type='button' id='btnHitung' value='Hitung Luas'
-                onclick='hitungLuas();'></td>
-        </tr>
-        <tr>
-            <td>Luas</td>
-            <td>:</td>
-            <td><input type='text' id='txtLuas' name='txtLuas'></td>
-        </tr>
-    </table>
-    <script type=text/javascript>
-        function hitungLuas() {
-            var alas = document.getElementById('txtAlas').value;
-            var tinggi = document.getElementById('txtTinggi').value;
-            var luas = (alas * tinggi) / 2;
-            document.getElementById('txtLuas').value = luas;
-        }
-    </script>
- 
+    <h1> Luas Segitiga </h1> <br>
+    <form method="POST" action="#">
+        Nilai Alas:<br>
+        <input type="number" name="txALAS" min="0" max="999" required><br><br>
+
+        <form method="POST" action="#">
+            Nilai Tinggi:<br>
+            <input type="number" name="txTINGGI" min="0" max="999" required><br><br>  
+            <textarea name="luas" id="luas">
+        <?php
+            
+        if(isset($_POST['hasil'])){
+                $alas    =$_POST["txALAS"];
+                $tinggi    =$_POST["txTINGGI"];
+                $luas = ($alas * $tinggi) /2 ;
+                echo $luas ;
+            }
+            ?>
+            </textarea>
+            <br> <input type="submit" name="hasil" value="hasil">
+            
+        </form>
+      
+        
+        
+    </form>
 </body>
 </html>
